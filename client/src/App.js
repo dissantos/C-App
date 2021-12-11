55be79e5fc4f4cfd4c249977bd7b83ae567108dd
bulgarellim <bulgarelli.mariana1@gmail.com>
2021-12-11 18:45:49 -0300

    Add footer/header draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import  Header  from './components/Header/Header';
+import  Footer  from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer></Footer>
     </div>
   );
 }
