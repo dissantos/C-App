d64b8ba64bb8f05a78ef34754e6e7812659f86e1
RenanSiman <renanvinte2@hotmail.com>
2021-12-14 07:42:35 -0300

    include grid in activities-page change patterns in calendar

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import lang from '@fullcalendar/core/locales/pt-br'
+import React from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import lang from "@fullcalendar/core/locales/pt-br";
+
+const titleFormat = {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+};
+
+const headerToolbar = {
+  left: "today",
+  center: "title",
+  right: "prev,next",
+};
+
+const events = {
+  title: "event 1",
+  start: "2021-12-13T01:00:00",
+  end: "2021-12-13T02:00:00",
+};
 
 export default class Calendar extends React.Component {
   render() {
     return (
       <FullCalendar
-        plugins={[ dayGridPlugin ]}
+        plugins={[dayGridPlugin]}
         initialView="dayGridWeek"
         locales={[lang]}
+        events={events}
+        titleFormat={titleFormat}
+        headerToolbar={headerToolbar}
+        eventBackgroundColor="#003869"
       />
-    )
+    );
   }
 }
