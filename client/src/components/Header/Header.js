59df5ea0e7eef5e3d99737fd43c654eca1d8a06d
bulgarellim <bulgarelli.mariana1@gmail.com>
2021-12-14 23:31:09 -0300

    Adjust header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,11 +3,11 @@ import "./Header.css";
 import AppBar from "@material-ui/core/AppBar";
 import { Nav } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Container from "react-bootstrap/Container";
 import SvgIcon from "@material-ui/core/SvgIcon";
 import IconButton from "@material-ui/core/IconButton";
-import ReactLogo from "../../images/account_circle_black_24dp.svg";
+import { MdAccountCircle } from "react-icons/md";
+
 const Header = () => (
   <div className="Header">
     <AppBar position="static">
@@ -32,11 +32,11 @@ const Header = () => (
               </Nav.Link>
             </Nav>
 
-            <Nav >
+            <Nav>
               Seu nome
-              <IconButton aria-label="Delete" className="menu-item">
-                <SvgIcon className="icon-img">
-                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z" />
+              <IconButton>
+                <SvgIcon className="material-icons">
+                  <MdAccountCircle />
                 </SvgIcon>
               </IconButton>
             </Nav>
