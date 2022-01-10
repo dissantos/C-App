3bbc4a532d76954c0534ceddc5e066284a7c5e18
bulgarellim <bulgarelli.mariana1@gmail.com>
2022-01-10 19:47:49 -0300

    Add news cards to Home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,6 +7,7 @@ import { Grid } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
+import NewsCard from "../NewsCard/NewsCard";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -15,6 +16,33 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+const newsCarsExample = [
+  {
+    category: "Graduação",
+    title:
+      "Edital seleciona alunos especiais para Mestrado em Automação e Sistemas",
+    dateDay: "23",
+    dateMonth: "dez",
+    url: "https://www.cefetmg.br/noticias/edital-seleciona-alunos-especiais-para-mestrado-",
+  },
+  {
+    category: "Graduação",
+    title:
+      "Edital seleciona alunos especiais para Mestrado em Automação e Sistemas",
+    dateDay: "23",
+    dateMonth: "dez",
+    url: "https://www.cefetmg.br/noticias/edital-seleciona-alunos-especiais-para-mestrado-",
+  },
+  {
+    category: "Graduação",
+    title:
+      "Edital seleciona alunos especiais para Mestrado em Automação e Sistemas",
+    dateDay: "23",
+    dateMonth: "dez",
+    url: "https://www.cefetmg.br/noticias/edital-seleciona-alunos-especiais-para-mestrado-",
+  },
+];
+
 class Home extends React.Component {
   render() {
     return (
@@ -22,14 +50,23 @@ class Home extends React.Component {
         <Box fluid sx={{ flexGrow: 1 }}>
           <Grid container spacing={2}>
             <Grid item xs={4}>
-              <Item>NOTICIAS</Item>
+              <Item>
+              <h4>Notícias</h4>
+              {newsCarsExample.map((newsCard, id) => {
+                  return (
+                    <Grid item key={id}>
+                      <NewsCard {...newsCard}></NewsCard>
+                    </Grid>
+                  );
+                })}
+              </Item>
             </Grid>
             <Grid item xs={4}>
               <Item>
                 <SuasAtividades></SuasAtividades>
               </Item>
             </Grid>
-            <Grid  item xs={4}>
+            <Grid item xs={4}>
               <Item>
                 <DadosAcademicos></DadosAcademicos>
               </Item>
