8b6b13c04cce7243844446af17aa6b4c21bd2c42
RenanSiman <renanvinte2@hotmail.com>
2022-01-10 23:02:45 -0300

    add Month Calendar component

---
+++ client/src/components/Calendar/MonthCalendar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import lang from "@fullcalendar/core/locales/pt-br";
+import './MonthCalendar.css'
+
+const titleFormat = {
+  year: "numeric",
+  month: "long",
+};
+
+const headerToolbar = {
+  left: "today",
+  center: "title",
+  right: "prev,next",
+};
+
+export default class Calendar extends React.Component {
+  render() {
+    return (
+      <FullCalendar
+        height={'35%'}
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        locales={[lang]}
+        titleFormat={titleFormat}
+        headerToolbar={headerToolbar}
+        eventBackgroundColor="#003869"
+      />
+    );
+  }
+}
