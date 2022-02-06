517869e36bfffd5b991f7db7451c9eec36cbab0d
RenanSiman <renanvinte2@hotmail.com>
2022-02-06 17:27:18 -0300

    delete unnecessary commented code

--- server/index.js
+++ server/index.js
@@ -13,17 +13,7 @@ app.post("/post", (req, res) => {
   console.log("Connected to React");
   res.redirect("/");
 });
-/*
-app
-  .route("/things/cars")
-  .get((req, res) => {})
-  .post((req, res) => {});
 
-app
-  .route("/things/cars/:carid")
-  .get((req, res) => {})
-  .put((req, res) => {});
-*/
 app.listen(PORT, (err) => {
   if (err) {
     return console.log("ERROR", err);
