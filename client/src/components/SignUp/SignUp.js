85d02db5139e19390a7cee6b18c452ca8482f34b
dissantos <disantosg18@gmail.com>
2022-02-13 03:49:42 -0300

    Add backend integration with signup

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -13,6 +13,8 @@ import Snackbar from '@mui/material/Snackbar';
 import { Typography } from '@material-ui/core';
 import Divider from '@mui/material/Divider';
 import { Slider } from '@mui/material';
+import setUser from "../../functions/setUser";
+
 
 
 const SignUp = () => {
@@ -25,6 +27,30 @@ const SignUp = () => {
   });
   const [images, setImages] = React.useState([]);
   const [errorAlert, setErrorAlert] = React.useState(false);
+  const [errorSignup, setErrorSignup] = React.useState(false);
+  const [url, setUrl] = React.useState('');
+  const [coef, setCoef] = React.useState(0);
+  const [cargaObg, setCargaObg] = React.useState(0);
+  const [cargaOpt, setCargaOpt] = React.useState(0);
+  const [cargaComp, setCargaComp] = React.useState(0);
+  
+  const uploadImage = () => {
+    const data = new FormData()
+    if(images && images[0]){
+      data.append("file", images[0].file)
+      data.append("upload_preset", "capp-web")
+      data.append("cloud_name","web-cefet")
+      fetch("  https://api.cloudinary.com/v1_1/web-cefet/image/upload",{
+        method:"post",
+        body: data
+      })
+      .then(resp => resp.json())
+      .then(data => {
+      setUrl(data.url)
+      })
+      .catch(err => console.log(err))
+    }
+  }
 
 
   const handleChange = (prop) => (event) => {
@@ -66,31 +92,36 @@ const SignUp = () => {
     setImages(imageList);
   };
 
-  const handleSignUp = () => {
+  const handleSignUp = async () => {
     const name = document.querySelector('#name-input').value;
     const user = document.querySelector('#user-input').value;
     const email = document.querySelector('#email-input').value;
     const matricula = document.querySelector('#matricula-input').value;
     const ano = document.querySelector('#ano-input').value;
     const curso = document.querySelector('#curso-input').value;
-    const coef = document.querySelector('#coef-input').value;
-    const cargaObg = document.querySelector('#carga-obg-input').value;
-    const cargaOpt = document.querySelector('#carga-opt-input').value;
-    const cargaComp = document.querySelector('#carga-comp-input').value;
-    if(name && user && email && ano && curso && matricula && coef && cargaObg && cargaOpt && cargaComp && values.password && values.confirmPassword && values.confirmPassword && !values.isIncorrectPassword) {
+    console.log(coef)
+    if(name && user && email && ano && curso && matricula  && values.password && values.confirmPassword && values.confirmPassword && !values.isIncorrectPassword) {
+      uploadImage();
       const cargaTot = cargaComp + cargaObg + cargaOpt;
-      const perc = cargaTot/4320;
-      //TO DO
-      const prof = {
-        nome: user,
-        nome_completo: name,
-        matricula: matricula,
-        curso: curso,
-        email: email,
-        ano_entrada: ano,
+      const perc = cargaTot/4380;
+
+      let res = await setUser(user, values.password, email, name, matricula, curso, ano, cargaOpt,cargaComp, cargaObg, coef, url);
+      if(res === 'error') {
+        setErrorSignup(true);
+      } else {
+        const prof = {
+          nome: user,
+          nome_completo: name,
+          matricula: matricula,
+          curso: curso,
+          email: email,
+          ano_entrada: ano,
+          url: url
+        }
+        localStorage.setItem('@C-app/login',prof);
+        window.location.href = '/principal';
       }
-      localStorage.setItem('@C-app/login',prof);
-      window.location.href = '/principal';
+   
     } else {
       setErrorAlert(true);
     }
@@ -102,6 +133,13 @@ const SignUp = () => {
         <Snackbar open={errorAlert} onClose={() => setErrorAlert(false)}>
           <Alert onClose={() => setErrorAlert(false)} color="error" variant="filled" >
             Erro no cadastro — preencha todos os campos e coloque as senhas iguais.
+          </Alert>
+        </Snackbar>
+      }
+      {errorSignup &&  
+        <Snackbar open={errorSignup} onClose={() => setErrorSignup(false)}>
+          <Alert onClose={() => setErrorAlert(false)} color="error" variant="filled" >
+            Erro no cadastro — matrícula ou nome de usuário já existe no sistema.
           </Alert>
         </Snackbar>
       }
@@ -131,13 +169,13 @@ const SignUp = () => {
         <TextField id="curso-input" label="Curso" variant="outlined" margin='normal'/>
         <TextField id="ano-input" label="Ano de entrada" variant="outlined" margin='normal'/>
         <Typography>Carga Horária Obrigatória:</Typography>
-        <Slider id="carga-obg-input" defaultValue={0} aria-label="Default" valueLabelDisplay="auto" min={0} max={4320}/>
+        <Slider onChange={(e, value) => setCargaObg(value)} id="carga-obg-input" defaultValue={0} aria-label="Default" valueLabelDisplay="auto" min={0} step={5} max={3390}/>
         <Typography>Carga Horária Optativa:</Typography>
-        <Slider id="carga-opt-input" defaultValue={0} aria-label="Default" valueLabelDisplay="auto" min={0} max={4320}/>
+        <Slider onChange={(e, value) => setCargaOpt(value)} id="carga-opt-input" defaultValue={0} aria-label="Default" valueLabelDisplay="auto" min={0} step={5} max={540}/>
         <Typography>Carga Horária Complementar:</Typography>
-        <Slider id="carga-comp-input" defaultValue={0} aria-label="Default" valueLabelDisplay="auto" min={0} max={4320}/>
+        <Slider onChange={(e, value) => setCargaComp(value)} id="carga-comp-input" defaultValue={0} aria-label="Default" valueLabelDisplay="auto" min={0} step={5} max={450}/>
         <Typography>Coeficiente:</Typography>
-        <Slider id="coef" defaultValue={0} aria-label="Default" valueLabelDisplay="auto" min={0} step={0.01} max={100}/>
+        <Slider onChange={(e, value) => setCoef(value)} id="coef" defaultValue={0} aria-label="Default" valueLabelDisplay="auto" min={0} step={0.01} max={100}/>
         
       </Box>
       <div>
@@ -164,7 +202,10 @@ const SignUp = () => {
                 <img src={image['data_url']} alt="" width="100px" />
                 <div className="image-item__btn-wrapper">
                   <Button onClick={() => onImageUpdate(index)}>Update</Button>
-                  <Button onClick={() => onImageRemove(index)}>Remove</Button>
+                  <Button onClick={() => {
+                    onImageRemove(index)
+                    setImages([])
+                  }}>Remove</Button>
                 </div>
               </div>
             ))}
